8a1fd3e721719f26d5647546a7123e949a7f2c9d
Hafiz-sustswe <hafiz.sust333@gmail.com>
2024-12-10 17:26:44 +0600

    ADD: Project Setup and Input Form

---
+++ src/components/InputForm/TodoForm.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { Input } from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea"
+
+
+interface Task {
+    id: string;
+    title: string;
+    description: string;
+    completed: boolean;
+    createdAt: Date;
+}
+
+const TodoForm: React.FC<{ addTask: (task: Task) => void }> = ({ addTask }) => {
+    const [title, setTitle] = useState<string>('');
+    const [description, setDescription] = useState<string>('');
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (title.trim() && description.trim()) {
+            const newTask: Task = {
+                id: "id - 1",
+                title,
+                description,
+                completed: false,
+                createdAt: new Date(),
+            };
+
+            addTask(newTask);
+            setTitle(title.trim());
+            setDescription(description.trim());
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="flex flex-col space-y-2">
+            <Input
+                type="text"
+                placeholder="Enter Your Task Title Here"
+                value={title}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)}
+                className="p-2 border border-gray-300 rounded bg-white"
+            />
+            <Textarea
+                placeholder="Enter Your Task Description"
+                value={description}
+                onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setDescription(e.target.value)}
+                className="p-2 border border-gray-300 rounded bg-white"
+            />
+            <Button
+                type="submit"
+                className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+            >
+                Add Task
+            </Button>
+        </form>
+    );
+};
+
+export default TodoForm;
